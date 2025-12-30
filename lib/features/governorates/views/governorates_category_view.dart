5cdd4e77a738b51cf1fb428360fd8348e85c07bb
Mohamed Ali <mohamed.ali01537@gmail.com>
2025-12-30 19:09:46 +0200

    Refactor governorates category view with improved layout and background image handling

--- lib/features/governorates/views/governorates_category_view.dart
+++ lib/features/governorates/views/governorates_category_view.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-
-import '../../../core/utils/app_text_styles.dart';
-import '../data/models/governorates_response_model.dart';
-import '../manager/governorates_cubit/governorates_cubit.dart';
 import 'widgets/governorates_category_widgets/governorates_category_view_body.dart';
 
 class GovernoratesCategoryView extends StatelessWidget {
@@ -12,16 +8,7 @@ class GovernoratesCategoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var cubit = GovernoratesCubit.get(context);
-    Governorate governorate = cubit.selectedGovernorate!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          governorate.name ?? '',
-          style: AppTextStyles.semiBold28.copyWith(color: Colors.black),
-        ),
-        centerTitle: true,
-      ),
       body: GovernoratesCategoryViewBody(),
     );
   }
--- lib/features/governorates/views/widgets/governorates_category_widgets/governorates_category_view_body.dart
+++ lib/features/governorates/views/widgets/governorates_category_widgets/governorates_category_view_body.dart
@@ -1,7 +1,12 @@
+import 'package:egy_go_guide/core/shared_widgets/cached_network_image_wrapper.dart';
+import 'package:egy_go_guide/core/utils/app_text_styles.dart';
+import 'package:egy_go_guide/features/governorates/manager/governorates_cubit/governorates_cubit.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/helper/my_responsive.dart';
+import '../../../../../core/utils/app_colors.dart';
 import 'governorates_category_grid_view.dart';
+
 
 class GovernoratesCategoryViewBody extends StatelessWidget {
   const GovernoratesCategoryViewBody({super.key});
@@ -10,44 +15,72 @@ class GovernoratesCategoryViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Positioned.fill(
-        //   child: Image.asset(
-        //     AppAssets.test,
-        //     fit: BoxFit.cover,
-        //   ),
-        // ),
-        // Positioned.fill(
-        //   child: Container(
-        //     decoration: BoxDecoration(
-        //       gradient: LinearGradient(
-        //         begin: Alignment.topCenter,
-        //         end: Alignment.bottomCenter,
-        //         colors: [
-        //           Colors.transparent,
-        //           Colors.black.withValues(alpha: .2),
-        //         ],
-        //         stops: [0.0, 0.0],
-        //       ),
-        //     ),
-        //   ),
-        // ),
-        Padding(
-          padding: MyResponsive.paddingSymmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(
-                height: MyResponsive.height(value: 100),
-              ),
 
-              // SizedBox(
-              //   height: MyResponsive.height(value: 150),
-              // ),
-              Expanded(
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: MyResponsive.height(value: 300),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius:
+                BorderRadius.circular(MyResponsive.width(value: 16)),
+              ),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: CachedNetworkImageWrapper(
+                      imagePath: GovernoratesCubit.get(context)
+                          .selectedGovernorate!
+                          .coverImage!,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withValues(alpha: .4),
+                          ],
+                          stops: [0.0, 0.0],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        height: MyResponsive.height(value: 40),
+                      ),
+                      Text(
+                        GovernoratesCubit.get(context)
+                            .selectedGovernorate!
+                            .name!,
+                        style: AppTextStyles.bold36
+                            .copyWith(color: AppColors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MyResponsive.height(value: 60),
+            ),
+            Expanded(
+              child: Padding(
+                padding: MyResponsive.paddingSymmetric(horizontal: 16),
                 child: GovernoratesCategoryGridView(),
               ),
-            ],
-          ),
+            ),
+          ],
         )
       ],
     );
